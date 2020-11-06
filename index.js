e59d0d6aa7a8e62b6f5d1350986da2ccd1362f02
Shen <shenbn@nci-02134517-ml.fios-router.home>
2020-11-06 14:34:39 -0500

    revert

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log(files)
 
 for(let i = 0; i < files.length; i++){
     let file = files[i]
-    if(file.indexOf('.csv') != -1){
+    /*if(file.indexOf('.csv') != -1){
         const fs = require('fs');
         fs.readdirSync('./jsons/').forEach(file => {
             if(file.includes('.json')){
@@ -22,10 +22,10 @@ for(let i = 0; i < files.length; i++){
         concept.readFile(file)
         i = files.length;
     }
-    else if(file.indexOf('.json') != -1){
+    else if(file.match(/[0-9]+.json/)){
         otherDirection.reverseRead()
         i = files.length;
-    }
+    }*/
     
 }
 
