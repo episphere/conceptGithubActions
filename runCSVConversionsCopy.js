239fe4679df802fc06369eff47a099477aa80c9a
Gene Barra <barragj@nih.gov>
2023-05-08 00:08:09 -0400

    add copy files

---
+++ runCSVConversionsCopy.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const concept = require('./conceptCopy');
+let file='./csvCopy/masterFile.csv' // Points to the csvCopy/masterFile
+concept.readFile(file)
+
+/*
+let fileName = "./csv/testing1.csv"
+let toReplace = fs.readFileSync(fileName,{encoding:'utf8', flag:'r'})
+//console.log(toReplace)
+toReplace = toReplace.replace(/�/g, "\"\"")
+fs.writeFileSync(fileName, toReplace)*/
