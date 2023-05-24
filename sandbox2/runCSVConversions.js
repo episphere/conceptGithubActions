abf84fab1c31d555e27480c22f55c4c3bf636573
Gene Barra <barragj@nih.gov>
2023-05-24 09:35:58 -0400

    add sandbox2

---
+++ sandbox2/runCSVConversions.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const concept = require('./concept');
+let file='./csv/checkTest.csv'
+concept.readFile(file)
+
+/*
+let fileName = "./csv/testing1.csv"
+let toReplace = fs.readFileSync(fileName,{encoding:'utf8', flag:'r'})
+//console.log(toReplace)
+toReplace = toReplace.replace(/�/g, "\"\"")
+fs.writeFileSync(fileName, toReplace)*/
